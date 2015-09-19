4123d0c955b847fccc8b3934fc9c833ecfb83984
Justin Silvestre <justinsilvestre@gmail.com>
2015-09-19 00:09:54 -0500

    Ready for sharing

---
+++ README.md
@@ -0,0 +1,19 @@
+Toki Pona Translator in React
+=====================
+
+Toki Pona is a [constructed language][cl] made up of about 120 words. Expressing your mind in Toki Pona is an exercise in simplifying one's thoughts.
+
+Try reading the Tower of Babel story in Toki Pona with the help of syntax highlighting and a popup dictionary.
+
+```
+git clone https://github.com/justinsilvestre/tokipona_react.git
+cd react-transform-boilerplate
+npm install
+npm start
+open http://localhost:3000
+```
+
+Built on top of Dan Abramov's [React Transform Boilerplate][rt].`
+
+[cl]: <http://www.theatlantic.com/technology/archive/2015/07/toki-pona-smallest-language/398363/>
+[rt]: <https://github.com/gaearon/react-transform-boilerplate>
--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-import React from 'react';
-import { connect, createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './components/App';
-import translateTokiponaApp from './reducers';
 import analysis from './analysis';
+import runTokiponaApp from './runApp.js'
 
-import { eachWithIndex as eachHeadWithIndex } from './heads'
-
-for (var i = 0; i < analysis.tree.length; i++) {
-	eachHeadWithIndex(analysis.tree[i], (head, phrase, headIndex) => {
-		phrase.h = headIndex;
-	});
-}
-
-const initialState = {
-	tpSentences: analysis.tree,
-  tpPhrases: analysis.tokipona_phrases,
-  enPhrases: analysis.english_phrases.sort((p1, p2) => p2.uses - p1.uses)
-};
-
-var store = createStore(translateTokiponaApp, initialState);
-
-React.render(
-	<Provider store={store}>
-	{() => <App />}
-	</Provider>,
-	document.getElementById('root')
-);
+runTokiponaApp(analysis, 'root');
